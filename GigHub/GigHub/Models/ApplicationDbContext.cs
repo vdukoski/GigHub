812955e95a180f9f4f5097285290270637e3a961
vdukoski <42813799+vdukoski@users.noreply.github.com>
2018-11-20 01:00:49 +0100

    Implemented followArtist

--- GigHub/GigHub/Models/ApplicationDbContext.cs
+++ GigHub/GigHub/Models/ApplicationDbContext.cs
@@ -11,6 +11,8 @@ namespace GigHub.Models
 
         public DbSet<Attendance> Attendances { get; set; }
 
+        public DbSet<Following> Followings { get; set; }
+
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
@@ -26,6 +28,16 @@ namespace GigHub.Models
             modelBuilder.Entity<Attendance>()
                 .HasRequired(a => a.Gig)
                 .WithMany()
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<ApplicationUser>()
+                .HasMany(u => u.Followers)
+                .WithRequired(f => f.Followee)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<ApplicationUser>()
+                .HasMany(u => u.Followees)
+                .WithRequired(f => f.Follower)
                 .WillCascadeOnDelete(false);
 
             base.OnModelCreating(modelBuilder);
